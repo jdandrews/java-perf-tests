4b0af2b78ee5c5f4a6c204bd80b6738133a98c73
Jerry Andrews <jerry@jrandrews.org>
2022-11-15 02:14:54 +0000

    JMX config

--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 CLASSPATH=build/libs/java-perf-tests.jar
+JMX_CONFIG="-Dcom.sun.management.jmxremote.port=7091 -Dcom.sun.management.jmxremote.password.file=/home/opc/jmxremote-password -Dcom.sun.management.jmxremote.ssl=false"
 
 gradle build
-java -classpath $CLASSPATH com.oracle.jsc.perf.Main
+
+echo java -classpath $CLASSPATH  $JMX_CONFIG com.oracle.jsc.perf.Main
+java -classpath $CLASSPATH  $JMX_CONFIG com.oracle.jsc.perf.Main
 
