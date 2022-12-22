14fb42b73a2addd50cf451e726763cfcf4587b8c
Jerry Andrews <jerry@jrandrews.org>
2022-12-22 18:40:50 +0000

    add start scripts

---
+++ ois-run-g1GC.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+MAX_HEAP=$1
+LEAK=$2
+
+CLASSPATH=build/libs/java-perf-tests.jar
+
+JMX_PASSWORD_FILE=~/jmxremote-password
+JMX=com.sun.management.jmxremote
+JMX_CONFIG="-D${JMX} -D${JMX}.port=7091 -D${JMX}.rmi.port=7091 -D${JMX}.ssl=false -D${JMX}.password.file=${JMX_PASSWORD_FILE}  -D${JMX}.host=localhost"
+
+OPTS="-Djava.net.preferIPv4Stack=true -Djava.rmi.server.hostname=localhost -Xmx${MAX_HEAP} -XX:+UseG1GC -XX:FlightRecorderOptions=stackdepth=128"
+
+gradle build
+
+echo ${OPTS} ${JMX_CONFIG} ${LEAK}
+java -classpath $CLASSPATH ${OPTS} ${JMX_CONFIG} com.oracle.jsc.perf.Main ${LEAK}
+
--- ois-run-serialGC.sh
+++ ois-run-serialGC.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+MAX_HEAP=$1
+LEAK=$2
 
 CLASSPATH=build/libs/java-perf-tests.jar
 
@@ -6,10 +8,10 @@ JMX_PASSWORD_FILE=~/jmxremote-password
 JMX=com.sun.management.jmxremote
 JMX_CONFIG="-D${JMX} -D${JMX}.port=7091 -D${JMX}.rmi.port=7091 -D${JMX}.ssl=false -D${JMX}.password.file=${JMX_PASSWORD_FILE}  -D${JMX}.host=localhost"
 
-OPTS="-Djava.net.preferIPv4Stack=true -Djava.rmi.server.hostname=localhost"
+OPTS="-Djava.net.preferIPv4Stack=true -Djava.rmi.server.hostname=localhost -Xmx${MAX_HEAP} -XX:+UseSerialGC -XX:FlightRecorderOptions=stackdepth=128"
 
 gradle build
 
-echo ${OPTS} ${JMX_CONFIG}
-java -classpath $CLASSPATH ${OPTS} $JMX_CONFIG com.oracle.jsc.perf.Main
+echo ${OPTS} ${JMX_CONFIG} ${LEAK}
+java -classpath $CLASSPATH ${OPTS} ${JMX_CONFIG} com.oracle.jsc.perf.Main ${LEAK}
 
